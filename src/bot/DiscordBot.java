2f68e61521831abfcb5e09467d9cae55da7147f3
Julien-Raphael Dautert <julien40@szut.uni-bremen.de>
2019-06-05 12:18:34 +0200

    beautifying

--- src/bot/DiscordBot.java
+++ src/bot/DiscordBot.java
@@ -31,6 +31,7 @@ public class DiscordBot extends ListenerAdapter {
 	String content;
 	FortniteCommand fncomm;
 	CsCommand cscomm;
+	static JDA jda;
 	private HashMap<String, Commands> commands = new HashMap<>();
 
 	public DiscordBot() {
@@ -45,48 +46,46 @@ public class DiscordBot extends ListenerAdapter {
 	public String getContent() {
 		return contentToSub;
 	}
-	
+
 	public static void main(String[] args)
-			throws LoginException, InterruptedException
-	{
-		JDA jda = new JDABuilder(AccountType.BOT)
+			throws LoginException, InterruptedException {
+		jda = new JDABuilder(AccountType.BOT)
 				.setToken("NTgxNDI1MTgxNjU0OTA4OTM4.XOfEiQ.mz7s3INZR2m0jRS5nn3fQRXSuok")
 				.addEventListener(new DiscordBot())
 				.build()
 				.awaitReady();
 	}
-		
+
 	@Override
-	public void onMessageReceived(MessageReceivedEvent event)
-	{
+	public void onMessageReceived(MessageReceivedEvent event) {
 		if (event.getAuthor().isBot()) return;
-		if (event.isFromType(ChannelType.TEXT))
-		{
+		if (event.isFromType(ChannelType.TEXT)) {
 			System.out.printf("[%s][%s] %#s: %s%n", event.getGuild().getName(),
 					event.getChannel().getName(), event.getAuthor(), event.getMessage().getContentDisplay());
 		}
-		else
-		{
+		else {
 			System.out.printf("[PM] %#s: %s%n", event.getAuthor(), event.getMessage().getContentDisplay());
 		}
+		
 		Message message = event.getMessage();
 		content = message.getContentRaw();
 		contentToSub = content.substring(1, content.length());
 		String[] commandsString = contentToSub.split(" ", 2);
 		MessageChannel channel = event.getChannel();
-		
+		System.out.println(jda.getPing());
+
 		if (parse.getCommand() != null) {
 			channel.sendMessage(parse.getCommand()).queue();
 		}
 		if (contentToSub.equalsIgnoreCase("help")) {
 			channel.sendMessage(parse.helpCommands()).queue();
 		}
-		
+
 		try {
-			 if (content.startsWith("!") && commandsString.length == 2) {
-				channel.sendMessage(commands.get(commandsString[0]).execute(commandsString[1])).queue();
+			if (content.startsWith("!") && commandsString.length == 2) {
+				channel.sendMessage(commands.get(commandsString[0]).execute(commandsString[1]) +"```fix\n"+ jda.getPing() + "ms\n```").queue();
 			}
-			
+
 		} catch (JSONException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
